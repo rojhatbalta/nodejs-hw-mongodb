c10acbae673d8b6d1a11e73ed330b64cb960675d
rojhatbalta <rojhatbalta@gmail.com>
2025-02-19 15:02:48 +0300

    contact model change update

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -2,6 +2,10 @@ import { model, Schema } from 'mongoose';
 
 const contactSchema = new Schema(
   {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
     name: {
       type: String,
       required: true,
